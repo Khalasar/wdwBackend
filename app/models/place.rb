867f0a9c32216745e263769b54c9b6d9217ca520
Andre Stuhrmann <andre_stuhrmann@web.de>
2014-08-15 14:17:17 +0200

    Create overwrite method to get json

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,18 @@ class Place < ActiveRecord::Base
   validates :lat, presence: true
   validates :lng, presence: true
   validates :title, presence: true
+
+  def as_json(options={})
+  	{
+  		"#{id}" => {
+  			"id" => id,
+  			"title" => title,
+  			"lat" => lat,
+  			"lng" => lng,
+  			"subtitle" => subtitle,
+  			"images_count" => 1
+  		}
+  	}
+  end
+
 end
