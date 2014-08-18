f14a2e2f4affbe79c70d12cfe8ce793863dd163c
André Stuhrmann <as@neopoly.de>
2014-08-18 16:35:32 +0200

    Changings on photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,8 +1,19 @@
 class Photo < ActiveRecord::Base
+  #attr_accessible :image
   #photo belongs to album
   belongs_to  :place
   #validations
-  validates   :place, presence: true
+  #validates   :place, presence: true
   # Photo uploader using carrierwave
-  mount_uploader :image, ImageUploader
+  mount_uploader :file, ImageUploader
+
+  def to_jq_upload
+    {
+      "name" => read_attribute(:file),
+      "size" => file.size,
+      "url" => file.url,
+      "delete_url" => "/photos/#{id}",
+      "delete_type" => "DELETE"
+    }
+  end
 end
