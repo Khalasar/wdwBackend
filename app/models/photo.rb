7223e231ef7f2fe5eadea44fbba1ae942bd5a32e
André Stuhrmann <as@neopoly.de>
2014-08-18 14:31:20 +0200

    Create image uploader and mount photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,8 @@
 class Photo < ActiveRecord::Base
+  #photo belongs to album
+  belongs_to  :place
+  #validations
+  validates   :place, presence: true
+  # Photo uploader using carrierwave
+  mount_uploader :image, ImageUploader
 end
