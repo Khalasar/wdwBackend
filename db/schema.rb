4f3bd3e40ee94dedf285b548466f6d6a73c52e19
André Stuhrmann <as@neopoly.de>
2014-08-18 14:03:16 +0200

    Create photo model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140812112426) do
+ActiveRecord::Schema.define(version: 20140818120045) do
+
+  create_table "photos", force: true do |t|
+    t.integer  "place_id"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "places", force: true do |t|
     t.string   "title",       null: false
