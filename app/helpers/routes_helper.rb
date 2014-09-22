059ea62f74577d9986fd7d2dac51d74654abeadd
André Stuhrmann <as@neopoly.de>
2014-09-22 13:16:26 +0200

    Remove not used files

--- app/helpers/routes_helper.rb
+++ app/helpers/routes_helper.rb
@@ -1,22 +1,20 @@
 module RoutesHelper
-	def places_selected
-		if @route.places
-			@route.places.map do |place|
-				place.id
-			end
-		else
-			""
-		end
+  def places_selected
+    if @route.places
+      @route.places.map do |place|
+        place.id
+      end
+    else
+      ''
+    end
+  end
 
-	end
+  def all_places
+    Place.all.collect do |p|
+      [p.title, p.id]
+    end
+  end
 
-	def all_places
-		Place.all.collect do |p|
-		  [p.title, p.id]
-		end
-	end
-
-	def method_name
-
-	end
+  def method_name
+  end
 end
