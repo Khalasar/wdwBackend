3268e5e7bbac02e46e58226d087e1824b52b9c35
André Stuhrmann <as@neopoly.de>
2015-03-19 14:01:48 +0100

    RuboCop: Style/SpaceAfterComma

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,11 +15,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :thumb do
-    process resize_to_fill: [200,200]
+    process resize_to_fill: [200, 200]
   end
 
   version :tiny do
-    process resize_to_fill: [50,50]
+    process resize_to_fill: [50, 50]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
